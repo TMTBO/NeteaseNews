85aacad497d602bbaaa77a0b2d34cccae97fe954
TobyoTenma <tmtbo@hotamil.com>
2016-06-14 09:34:57 +0800

    Creat project demo

---
+++ NeteaseNews/NeteaseNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NeteaseNews
+//
+//  Created by TobyoTenma on 16/6/14.
+//  Copyright © 2016年 TobyoTenma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
